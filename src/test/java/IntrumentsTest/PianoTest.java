8aeb9f7d0d449f4973749bcf986a97d01c72c8b0
chloe alcorn <ca.alcorn@outlook.com>
2018-10-19 14:59:32 +0100

    piano created and tests passing

--- src/test/java/IntrumentsTest/PianoTest.java
+++ src/test/java/IntrumentsTest/PianoTest.java
@@ -12,7 +12,7 @@ public class PianoTest {
 
     @Before
     public void before(){
-        piano = new Piano(InstrumentType.PERCUSSION, "wood","Black","Yanama",5000.0,8000.0,36, 52,"Plink Plonk", "Sugar Cane" );
+        piano = new Piano(InstrumentType.PERCUSSION, "wood","Black","Yamaha",5000.0,8000.0,36, 52,"Plink Plonk", "Sugar Cane" );
     }
 
     @Test
@@ -20,5 +20,53 @@ public class PianoTest {
         assertEquals(InstrumentType.PERCUSSION, piano.getType());
     }
 
+    @Test
+    public void pianoHasMaterial(){
+        assertEquals("wood", piano.getMaterial());
 
+    }
+
+    @Test
+    public void pianoHasColour(){
+        assertEquals("Black", piano.getColour());
+    }
+
+    @Test
+    public void pianoHasMake(){
+        assertEquals("Yamaha", piano.getMake());
+    }
+
+    @Test
+    public void pianoHasCost(){
+        assertEquals(5000.0, piano.getCost(), 0.01);
+    }
+
+    @Test
+    public void pianoHasSellPrice(){
+        assertEquals(8000.0, piano.getSell(), 0.01);
+    }
+
+    @Test
+    public void numberOFWhiteKeys(){
+        assertEquals(52, piano.getNumberOfWhiteKeys());
+    }
+    @Test
+    public void numberOfBlackKeys(){
+        assertEquals(36, piano.getNumberOfBlackKeys(),0.01);
+    }
+
+    @Test
+    public void canGetTotalNumberOfKeys(){
+        assertEquals(88, piano.getTotalNumberOfKeys());
+    }
+
+    @Test
+    public void canGetKeysMaterial(){
+        assertEquals("Sugar Cane", piano.getKeysMaterial());
+    }
+
+    @Test
+    public void canPlaySong(){
+        assertEquals("Plink Plonk", piano.canPlay());
+    }
 }
